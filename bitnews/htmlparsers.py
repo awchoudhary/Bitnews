4e377709f3716a08e227e4e3e095dfd4fa7d8720
Awaes Choudhary <acedu77@gmail.com>
2018-08-10 22:28:35 -0500

    Create parser for coindesk

---
+++ bitnews/htmlparsers.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+from collections import namedtuple
+
+dispatcher = {"coindesk": parse_coindesk}
+
+def parse_coindesk(html):
+    soup = BeautifulSoup(html, "html.parser")
+    link_tags = soup.findAll("a", {"class": "fade"})
+    articles = []
+    Article = namedtuple('Article', 'title link')
+
+    for tag in link_tags:
+        article_info = Article(tag["title"], tag["href"])
+        articles.append(article_info)
+
+    return articles
+
+
+
