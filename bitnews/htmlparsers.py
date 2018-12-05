1f6cbb9b14857f8e6a27bca33566a96888848016
Awaes Choudhary <acedu77@gmail.com>
2018-12-05 14:27:02 -0800

    Fix coindesk parser

--- bitnews/htmlparsers.py
+++ bitnews/htmlparsers.py
@@ -3,21 +3,17 @@ from collections import namedtuple
 
 def parse_coindesk(html):
     soup = BeautifulSoup(html, "html.parser")
-    post_divs = soup.find_all("div", {"class": "post-info"})
-    link_tags = []
-
-    for div in post_divs:
-        link_tags.append(div.find("a", {"class": "fade"}))
+    article_link_elements = soup.find_all("a", {"class": "stream-article"})
 
     articles = []
     Article = namedtuple('Article', 'title link')
 
-    for tag in link_tags:
-        article_info = Article(tag.text.strip(), tag["href"])
+    for tag in article_link_elements:
+        article_info = Article(tag["title"], tag["href"])
         articles.append(article_info)
 
     return articles
-
+    
 dispatcher = {"coindesk": parse_coindesk}
 
 
