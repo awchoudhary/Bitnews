dbfc87f2cd8bdbfb178333ec2137eb92357d9203
Awaes Choudhary <acedu77@gmail.com>
2018-12-07 19:01:12 -0800

    Added search bar to page

--- bitnews/routes.py
+++ bitnews/routes.py
@@ -1,29 +1,17 @@
-from yattag import Doc
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 import articlescraper
 import articlesearcher
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/search')
-def hello_world():
+def search():
     query_string = request.args.get('querystring')
     all_articles = articlescraper.get_all_articles()
     found_articles = articlesearcher.get_articles_containing_words(all_articles, query_string.split())
-    response = generate_html(found_articles)
 
-    return response
-
-        
-def generate_html(articles):
-    doc, tag, text = Doc().tagtext()
-    html = []
-
-    for article in articles:
-        with tag("a", href = article.link):
-            text(article.title + " - " + article.source)
-        
-        doc.stag('br')
-
-    return doc.getvalue()
+    return render_template('index.html', articles = found_articles)
