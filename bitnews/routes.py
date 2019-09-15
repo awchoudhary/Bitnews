fa472529db7e13abe139ac532d257449fe9f3619
Awaes Choudhary <acedu77@gmail.com>
2019-09-14 22:27:10 -0700

    styling updates

--- bitnews/routes.py
+++ bitnews/routes.py
@@ -6,12 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    all_articles = articlescraper.get_all_articles()
+    return render_template('index.html', articles = all_articles)
 
 @app.route('/search')
 def search():
-    query_string = request.args.get('querystring')
+    query_string = request.args.get('q')
     all_articles = articlescraper.get_all_articles()
+
+    if query_string == '':
+        return render_template('index.html', articles = all_articles)
+
     found_articles = articlesearcher.get_articles_containing_words(all_articles, query_string.split())
 
     return render_template('index.html', articles = found_articles)
