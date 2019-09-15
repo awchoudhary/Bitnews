c1b74787c47179c647c4faa20050d273c57e9ce5
Awaes Choudhary <acedu77@gmail.com>
2019-09-14 22:33:36 -0700

    scrape more articles

--- bitnews/htmlparsers.py
+++ bitnews/htmlparsers.py
@@ -3,7 +3,10 @@ from collections import namedtuple
 
 def parse_coindesk(html):
     soup = BeautifulSoup(html, "html.parser")
-    article_link_elements = soup.find_all("a", {"class": "stream-article"})
+    article_link_elements = []
+    article_link_elements += soup.find_all("a", {"class": "stream-article"})
+    article_link_elements += soup.find_all("a", {"class": "top-article"})
+    article_link_elements += soup.find_all("a", {"class": "feature"})
 
     articles = []
     Article = namedtuple('Article', 'source title link')
